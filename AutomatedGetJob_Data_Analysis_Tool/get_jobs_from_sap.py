38f4a3b762e48a95b9840cc7ee1c705846c81ad7
E. Abdiel Rodriguez <abdiel.rodriguez@live.com>
2020-09-30 09:01:50 -0400

    Add files via upload

---
+++ AutomatedGetJob_Data_Analysis_Tool/get_jobs_from_sap.py
@@ -0,0 +1,272 @@
+import pyautogui as pgui
+import time as t
+import pyperclip
+import datetime as DT
+#import os, sys
+import bs4
+import csv
+
+
+def get_jobs(filepath, filename, username, serversid):
+    # open SAP
+    pgui.hotkey("win")
+    t.sleep(5)
+    pgui.typewrite("SAP Logon")
+    pgui.hotkey("Enter")
+    # give enough time for sap logon pad to open; default 5 seconds
+    t.sleep(3)
+    # after loading, press ctrl+f to go to search bar
+    pgui.hotkey("ctrl", "f")
+    t.sleep(2)
+    backcounter = 0
+    while backcounter < 10:
+        pgui.hotkey("backspace")
+        backcounter += 1
+
+    # wait a few seconds to move cursor - then type server or SID name
+    t.sleep(2)
+    pgui.typewrite(serversid)
+    pgui.hotkey('enter')
+    # wait a few seconds for buffer to finish loading, then press enter to login
+    t.sleep(2)
+    pgui.typewrite('010')
+    pgui.hotkey("Tab")
+    pgui.typewrite(username)
+    # submit a few times in case an informational popup appears
+    entersleep = 0
+    while entersleep < 4:
+        t.sleep(1)
+        pgui.hotkey('enter')
+        entersleep += 1
+
+    # SAP Automation Steps start here ---
+    pgui.hotkey('ctrl', '/')
+    t.sleep(1)
+    pgui.typewrite('/nsm37')
+    pgui.hotkey('enter')
+    t.sleep(1)
+    pgui.typewrite('Z80*')
+    pgui.hotkey('tab')
+    t.sleep(1)
+    pgui.typewrite('*')
+    pgui.hotkey('tab')  # move from user name field
+    pgui.hotkey('tab')  # move from shed field
+    pgui.hotkey('tab')  # move from released field
+    pgui.hotkey('tab')  # move from ready field
+    pgui.hotkey('tab')  # move from active field
+    pgui.hotkey('tab')  # move from finished field
+    pgui.hotkey('tab')  # move from cancelled field
+    t.sleep(1)  # give a few seconds to ctrl - copy
+    pgui.hotkey('ctrl', 'a')  # select date
+    pgui.hotkey('ctrl', 'c')  # copy from date
+    t.sleep(1)
+
+    # Start- Input Date formatting
+    paste_date = pyperclip.paste()  # paste date in MMDDYYYY
+    date = DT.datetime.strptime(paste_date, '%m/%d/%Y')  # convert to date object
+    minus_days = DT.timedelta(days=6)  # a day object to be used to substract from entry
+    newdate = date - minus_days  # create a newdate object with entry date minus the days specified
+    newdate_formatted = newdate.strftime('%m/%d/%Y')  # new formatted day should be pasted date - 6 days
+    filenamedate = newdate.strftime('%m%d%Y')
+    # End- Input Date formatting
+
+    # Continue automation
+    t.sleep(1)
+    pgui.hotkey('ctrl', 'a')
+    pgui.typewrite(newdate_formatted)
+    pgui.hotkey('f8')
+    t.sleep(1)
+
+    # at this point we should be at the sm37 job results screen
+    pgui.hotkey('altleft', 'a')  # go to 'extras'
+    t.sleep(1)
+    pgui.typewrite('e')
+    t.sleep(1)
+    pgui.typewrite('l')
+    t.sleep(1)
+    pgui.hotkey('down')
+    pgui.hotkey('down')
+    pgui.hotkey('down')
+    pgui.hotkey('enter')  # save to html
+    t.sleep(1)
+    pgui.hotkey('up')
+    pgui.hotkey('ctrl', 'a')
+    t.sleep(1)
+    path = filepath
+    pgui.typewrite(path)
+    t.sleep(1)
+    pgui.hotkey('tab')
+    pgui.typewrite(f'{filename}{filenamedate}.html')
+    t.sleep(1)
+    pgui.hotkey('enter')
+    t.sleep(3)
+    pgui.hotkey('ctrl', 's')
+    t.sleep(1)
+    pgui.hotkey('alt', 'f4')
+    t.sleep(1)
+    pgui.hotkey('tab')
+    t.sleep(1)
+    pgui.hotkey('enter')  # close sap
+
+    # Data processing step here
+    data = []
+    soup = bs4.BeautifulSoup(open(f'{filename}{filenamedate}.html'), 'html.parser')
+    table = soup.find('table', attrs={'class': 'list'})
+    table_body = table.find('tbody')
+    rows = table_body.find_all('tr')
+    for row in rows:
+        cols = row.find_all('td')
+        cols = [ele.text.strip() for ele in cols]
+        data.append([ele for ele in cols if ele])
+    # data here has the values from the table in the html
+    # save this to csv for easier data analysis later
+    with open(f'{filename}{filenamedate}.txt', 'w', newline='') as fou:
+        cw = csv.writer(fou)
+        cw.writerows(data)
+    return data
+
+
+def error_logs(filepath, filename, username, serversid):
+    # Data processing step here
+    data = []
+    soup = bs4.BeautifulSoup(open(f'{filename}.html'), 'html.parser')
+    table = soup.find('table', attrs={'class': 'list'})
+    table_body = table.find('tbody')
+    rows = table_body.find_all('tr')
+    for row in rows:
+        cols = row.find_all('td')
+        cols = [ele.text.strip() for ele in cols]
+        data.append([ele for ele in cols if ele])
+    # data here has the values from the table in the html
+    # save this to csv for easier data analysis later
+    with open(f'{filename}.txt', 'w', newline='') as fou:
+        cw = csv.writer(fou)
+        cw.writerows(data)
+
+    # open SAP once
+    pgui.hotkey("win")
+    t.sleep(5)
+    pgui.typewrite("SAP Logon")
+    pgui.hotkey("Enter")
+    # give enough time for sap logon pad to open; default 5 seconds
+    t.sleep(3)
+    # after loading, press ctrl+f to go to search bar
+    pgui.hotkey("ctrl", "f")
+    t.sleep(2)
+    backcounter = 0
+    while backcounter < 10:
+        pgui.hotkey("backspace")
+        backcounter += 1
+
+    # wait a few seconds to move cursor - then type server or SID name
+    t.sleep(2)
+    pgui.typewrite(serversid)
+    pgui.hotkey('enter')
+    # wait a few seconds for buffer to finish loading, then press enter to login
+    t.sleep(2)
+    pgui.typewrite('010')
+    pgui.hotkey("Tab")
+    pgui.typewrite(username)
+    # submit a few times in case an informational popup appears
+    entersleep = 0
+    while entersleep < 4:
+        t.sleep(1)
+        pgui.hotkey('enter')
+        entersleep += 1
+
+    # iterate through the csv file where lines = cancelled
+    # perform screen loop to get separate files for each 'cancelled' job
+    for job in data:
+        if job[2] == 'Canceled':
+
+            job_name = job[0]
+            start_date = job[3]
+            start_time = job[4]
+
+            # Start- Input Date formatting
+            date = DT.datetime.strptime(start_date, '%m/%d/%Y')  # convert to date object
+            filenamedate = date.strftime('%m%d%Y')
+            # End- Input Date formatting
+
+            # SAP Automation Steps start here ---
+            pgui.hotkey('ctrl', '/')
+            t.sleep(1)
+            pgui.typewrite('/nsm37')
+            pgui.hotkey('enter')
+            t.sleep(1)
+            pgui.typewrite(job_name)  # jobname here
+            pgui.hotkey('tab')
+            t.sleep(1)
+            pgui.typewrite('*')
+            pgui.hotkey('tab')  # move from user name field
+            pgui.hotkey('tab')  # move from shed field
+            pgui.hotkey('space')  # clear released field
+            pgui.hotkey('tab')  # move from released field
+            pgui.hotkey('space')  # clear ready field
+            pgui.hotkey('tab')  # move from ready field
+            pgui.hotkey('space')  # clear active field
+            pgui.hotkey('tab')  # move from active field
+            pgui.hotkey('space')  # clear finished field
+            pgui.hotkey('tab')  # move from finished field
+            pgui.hotkey('tab')  # move from cancelled field
+            t.sleep(1)  # give a few seconds to ctrl - copy
+            pgui.hotkey('ctrl', 'a')  # select date
+            pgui.hotkey('ctrl', 'c')  # copy from date
+            t.sleep(1)
+
+            t.sleep(1)
+            pgui.hotkey('ctrl', 'a')
+            pgui.typewrite(start_date)
+            pgui.hotkey('tab')
+            pgui.hotkey('ctrl', 'a')
+            pgui.typewrite(start_date)
+            pgui.hotkey('tab')
+            pgui.typewrite(start_time)
+            pgui.hotkey('tab')
+            pgui.typewrite(start_time)
+            pgui.hotkey('f8')
+            t.sleep(1)
+
+            # at this point we should be at the sm37 job results screen
+            pgui.hotkey('f5')  # go to 'extras'
+            t.sleep(1)
+            pgui.hotkey('ctrl', 'shift', 'f11')
+            t.sleep(1)
+            pgui.hotkey('shift', 'f6')
+            t.sleep(1)
+            pgui.hotkey('down')
+            pgui.hotkey('down')
+            pgui.hotkey('down')
+            pgui.hotkey('enter')  # save to html
+            t.sleep(1)
+            pgui.hotkey('up')
+            pgui.hotkey('ctrl', 'a')
+            t.sleep(1)
+            path = filepath
+            pgui.typewrite(f'{path}/{filenamedate}')
+            t.sleep(1)
+            pgui.hotkey('tab')
+            pgui.typewrite(f'{job_name}_{filenamedate}.html')
+            t.sleep(1)
+            pgui.hotkey('enter')
+            t.sleep(5)
+            pgui.hotkey('ctrl', 's')
+            t.sleep(1)
+
+            # Data processing step here
+            data = []
+            soup = bs4.BeautifulSoup(open(f'{filepath}/{filenamedate}/{job_name}_{filenamedate}.html'), 'html.parser')
+            table = soup.find('table', attrs={'class': 'list'})
+            table_body = table.find('tbody')
+            rows = table_body.find_all('tr')
+            for row in rows:
+                cols = row.find_all('td')
+                cols = [ele.text.strip() for ele in cols]
+                data.append([ele for ele in cols if ele])
+            # data here has the values from the table in the html
+            # save this to csv for easier data analysis later
+            with open(f'{filepath}/{filenamedate}/{job_name}_{filenamedate}.txt', 'w', newline='') as fou:
+                cw = csv.writer(fou)
+                cw.writerows(data)
+        else:
+            continue
---
+++ AutomatedGetJob_Data_Analysis_Tool/job_log_ui.py
@@ -0,0 +1,82 @@
+from tkinter import *
+import get_jobs_from_sap as jobslist
+import os, sys
+import datetime
+
+
+def get_jobs():
+    if len(v_filepath.get()) == 0:
+        v_filepath.set(str(os.path.dirname(sys.argv[0])))
+    if len(v_filename.get()) == 0:
+        v_filename.set('JOBLIST_')
+    if len(v_user.get()) == 0:
+        v_user.set(str(os.getlogin()))
+    if len(v_sid.get()) == 0:
+        v_sid.set('QTA')
+
+    # the function will run the automation and return a list of tuples with jobs
+    list_jobs = jobslist.get_jobs(v_filepath.get(), v_filename.get(), v_user.get(), v_sid.get())
+    print(list_jobs)
+    print('Job list created')
+
+
+def get_canceled():
+    if len(v_filepath.get()) == 0:
+        v_filepath.set(str(os.path.dirname(sys.argv[0])))
+    if len(v_filename.get()) == 0:
+        minus_days = datetime.timedelta(days=6)  # a day object to be used to substract from entry
+        default_date = datetime.datetime.today()
+        newdate = default_date - minus_days  # create a newdate object with entry date minus the days specified
+        newdate = newdate.strftime('%m%d%Y')
+        v_filename.set(f'JOBLIST_{newdate}')
+    if len(v_user.get()) == 0:
+        v_user.set(str(os.getlogin()))
+    if len(v_sid.get()) == 0:
+        v_sid.set('QTA')
+
+    jobslist.error_logs(v_filepath.get(), v_filename.get(), v_user.get(), v_sid.get())
+    print('Canceled list created')
+
+
+window = Tk()
+window.wm_title('Job Log Importer')
+
+lbl_user = Label(window, text='User Name: ')
+lbl_user.grid(row=0, column=0)
+
+lbl_sid = Label(window, text='SID/Server: ')
+lbl_sid.grid(row=0, column=2)
+
+lbl_filepath = Label(window, text='Save to: ')
+lbl_filepath.grid(row=1, column=0)
+
+lbl_filename = Label(window, text='FileName (w/o ext.): ')
+lbl_filename.grid(row=1, column=2)
+
+# v_optional = IntVar()
+# chk_optional = Checkbutton(window, text='Get Canceled Jobs', variable=v_optional)
+# chk_optional.grid(row=2, column=3)
+
+v_user = StringVar()
+p_user = Entry(window, textvariable=v_user)
+p_user.grid(row=0, column=1)
+
+v_sid = StringVar()
+p_sid = Entry(window, textvariable=v_sid)
+p_sid.grid(row=0, column=3)
+
+v_filepath = StringVar()
+p_filepath = Entry(window, textvariable=v_filepath)
+p_filepath.grid(row=1, column=1)
+
+v_filename = StringVar()
+p_filename = Entry(window, textvariable=v_filename)
+p_filename.grid(row=1, column=3)
+
+btn_getjobs = Button(window, text='Get Jobs', width=12, command=get_jobs)
+btn_getjobs.grid(row=3, column=1)
+
+btn_getcanc = Button(window, text='Get Canceled', width=12, command=get_canceled)
+btn_getcanc.grid(row=3, column=2)
+
+window.mainloop()
